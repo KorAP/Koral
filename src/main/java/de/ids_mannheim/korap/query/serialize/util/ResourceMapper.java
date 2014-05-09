5e17fe2ab3c471f4ea737aa3e0477db79dc5c7b2
Michael Hanl <micha.hanl@gmail.com>
2014-05-09 15:51:02 +0000

    more performant static map descriptors

--- src/main/java/de/ids_mannheim/korap/query/serialize/util/ResourceMapper.java
+++ src/main/java/de/ids_mannheim/korap/query/serialize/util/ResourceMapper.java
@@ -1,22 +1,27 @@
 package de.ids_mannheim.korap.query.serialize.util;
 
 import java.util.HashMap;
+import java.util.Map;
 
 
 public class ResourceMapper {
 
-	public static String descriptor2policy(String descriptor) {
-		HashMap<String, String> map = new HashMap<String, String>();
-		map.put("surface", "t");
-		map.put("lemma", "l");
-		map.put("pos", "p");
-		map.put("const", "c");
-		map.put("dep", "d");
-		map.put("morph", "msd");
-		if (map.containsKey(descriptor)) 
-			return map.get(descriptor);
-		else
-			return descriptor;
-	}
+    private static final Map<String, String> map = new HashMap<>();
+
+    static {
+        map.put("surface", "t");
+        map.put("lemma", "l");
+        map.put("pos", "p");
+        map.put("const", "c");
+        map.put("dep", "d");
+        map.put("morph", "msd");
+    }
+
+    public static String descriptor2policy(String descriptor) {
+        if (map.containsKey(descriptor))
+            return map.get(descriptor);
+        else
+            return descriptor;
+    }
 
 }
