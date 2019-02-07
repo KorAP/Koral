87caf80cfba5997ecbd05806dd186ee49cadffad
margaretha <margaretha@ids-mannheim.de>
2019-02-07 16:38:41 +0100

    Fixed poliqarp layer bug (#52) & replaced log4j with log4j2.
    Change-Id: I29e34b51ae71aaf98e59d206d54f8c640824ef2e

--- src/main/java/de/ids_mannheim/korap/query/serialize/PoliqarpPlusQueryProcessor.java
+++ src/main/java/de/ids_mannheim/korap/query/serialize/PoliqarpPlusQueryProcessor.java
@@ -41,7 +41,7 @@ import java.util.*;
  */
 public class PoliqarpPlusQueryProcessor extends Antlr4AbstractQueryProcessor {
 
-    private static final boolean DEBUG = false;
+    private static final boolean DEBUG = true;
     private static Logger log = LoggerFactory
             .getLogger(PoliqarpPlusQueryProcessor.class);
     private int classCounter = 1;
@@ -867,23 +867,34 @@ public class PoliqarpPlusQueryProcessor extends Antlr4AbstractQueryProcessor {
             }
             // process possible flags
             if (flagNode != null) {
-                ArrayList<String> flags = new ArrayList<String>();
                 // substring removes leading slash
                 String flag = getNodeCat(flagNode.getChild(0)).substring(1);
-
-                if (flag.contains("i"))
-                    flags.add("flags:caseInsensitive");
-                if (flag.contains("x")) {
-                    if (!isRegex) {
-                        key = QueryUtils.escapeRegexSpecialChars(key);
-                    }
-                    // flag 'x' allows submatches:
-                    // overwrite key with appended .*?
-                    term.put("key", ".*?" + key + ".*?"); //
-                    term.put("type", "type:regex");
+                
+                // EM: handling no layer
+                if (layerNode == null) {
+                    term.put("layer", flag);
                 }
-                if (!flags.isEmpty()) {
-                    term.put("flags", flags);
+                else if (node.getChild(1).equals(flagNode)){
+                    String layer = (String) term.get("layer");
+                    term.put("layer", flag+layer);
+                }
+                else {
+                    //
+                    ArrayList<String> flags = new ArrayList<String>();
+
+                    if (flag.contains("i")) flags.add("flags:caseInsensitive");
+                    if (flag.contains("x")) {
+                        if (!isRegex) {
+                            key = QueryUtils.escapeRegexSpecialChars(key);
+                        }
+                        // flag 'x' allows submatches:
+                        // overwrite key with appended .*?
+                        term.put("key", ".*?" + key + ".*?"); //
+                        term.put("type", "type:regex");
+                    }
+                    if (!flags.isEmpty()) {
+                        term.put("flags", flags);
+                    }
                 }
             }
             return term;
@@ -1042,11 +1053,13 @@ public class PoliqarpPlusQueryProcessor extends Antlr4AbstractQueryProcessor {
         Antlr4DescriptiveErrorListener errorListener = new Antlr4DescriptiveErrorListener(
                 query);
         // Like p. 111
+        CommonTokenStream tokens = null;
         try {
             // Tokenize input data
             ANTLRInputStream input = new ANTLRInputStream(query);
             lexer.setInputStream(input);
-            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            tokens = new CommonTokenStream(lexer);
+            
             parser = new PoliqarpPlusParser(tokens);
 
             // Don't throw out erroneous stuff
