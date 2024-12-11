7a4bff7e2a35ef0c03ebf41ee0b1ba3634f13b2f
Franck Bodmer <bodmer@ids-mannheim.de>
2024-12-11 16:47:06 +0100

    opLEM: Review corrections.
    opLEM: accepts and generates lemma Search Options containing wildcards correctly. Rejects expr. if wildcards are inside the lemma string.
    
    opLEM: check or reject wildcards.
    
    opLEM: reject wildcards with error message.
    
    opLEM: test returned error code.
    
    opLEM: removed debug output.
    
    opLEM: refactoring error message methodes to StatusCodes.java.
    Change-Id: I01dd9dd9a61acee095d6f997fde4f4c3e5822b5d
    Reviewed-on: https://korap.ids-mannheim.de/gerrit/c/KorAP/Koral/+/9096
    Reviewed-by: <bodmer@ids-mannheim.de>

--- src/main/java/de/ids_mannheim/korap/query/serialize/util/StatusCodes.java
+++ src/main/java/de/ids_mannheim/korap/query/serialize/util/StatusCodes.java
@@ -1,6 +1,16 @@
 package de.ids_mannheim.korap.query.serialize.util;
 
+import org.antlr.runtime.CommonToken;
+import org.antlr.runtime.tree.CommonTree;
+
+import de.ids_mannheim.korap.query.parse.cosmas.c2ps_opPROX;
+
 public class StatusCodes {
+	
+	// type of an Error CommonToken:
+	public final static int typeERROR = 1; 
+	
+	// error codes:
     public final static int NO_QUERY = 301;
     public final static int MALFORMED_QUERY = 302;
     public final static int DEPRECATED_QUERY_ELEMENT = 303;
@@ -23,4 +33,126 @@ public class StatusCodes {
 	public final static int ERR_PROX_VAL_TOOGREAT 	= 324;
 	public final static int ERR_PROX_DIR_TOOGREAT 	= 325;
 	public final static int ERR_PROX_WRONG_CHARS	= 326;
+	
+	// error codes for WF and LEM syntax errors:
+	public final static int ERR_LEM_WILDCARDS		= 350;
+	
+	// constants for message languages
+	
+	public static final int MLANG_ENGLISH = 0;
+	public static final int MLANG_GERMAN  = 1;
+	
+	public static int messLang = MLANG_GERMAN; // default.
+	
+	   /**
+		 * buildErrorTree(): 
+		 * @param text = part of the query that contains an error.
+		 * @param errCode
+		 * @param pos = position of the expression where the error occurs.
+		 * @return CommonTree (ERROR pos errCode errMess).
+		 */
+		
+	public static CommonTree buildErrorTree(String text, int errCode, int pos)
+		{
+		CommonTree
+			errorNode = new CommonTree(new CommonToken(typeERROR, "ERROR"));
+		CommonTree
+			errorPos  = new CommonTree(new CommonToken(typeERROR, String.valueOf(pos)));
+		CommonTree
+			errorCode = new CommonTree(new CommonToken(typeERROR, String.valueOf(errCode)));
+		CommonTree
+			errorMes;
+		String
+			mess;
+		
+		mess 	 = getErrMess(errCode, messLang, text);
+		errorMes = new CommonTree(new CommonToken(typeERROR, mess));
+		
+		// new:
+		errorNode.addChild(errorPos);
+		errorNode.addChild(errorCode);
+		errorNode.addChild(errorMes);
+		
+		return errorNode;
+		}
+		
+	private static String getErrMessEN(int errCode, String text)
+		
+		{
+		switch( errCode )
+			{
+		case ERR_PROX_MEAS_NULL:
+			return String.format("Proximity operator at '%s': one of the following prox. types is missing: w,s,p!", text);
+
+		case ERR_PROX_MEAS_TOOGREAT:
+			return String.format("Proximity operator at '%s': Please, specify only 1 of the following prox. types: w,s,p! " +
+								 "It is possible to specify several at once by separating them with a ','. E.g.: ' /+w2,s2,p0 '.", text);
+			
+		case ERR_PROX_VAL_NULL:
+			return String.format("Proximity operator at '%s': please specify a numerical value for the distance. E.g. ' /+w5 '.", text);
+			
+		case ERR_PROX_VAL_TOOGREAT:
+			return String.format("Proximity operator at '%s': please specify only 1 distance value. E.g. ' /+w5 '.", text);
+			
+		case ERR_PROX_DIR_TOOGREAT:
+			return String.format("Proximity operator at '%s': please specify either '+' or '-' or none of them for the direction.", text);
+			
+		case ERR_PROX_WRONG_CHARS:
+			return String.format("Proximity operator at '%s': unknown proximity options!", text);
+
+		case ERR_LEM_WILDCARDS:
+			return String.format("Lemma operator at '%s': wildcards (?*+) are not allowed inside a lemma.", text);
+			
+		default:
+			return String.format("Proximity operator at '%s': unknown error. The correct syntax looks like this: E.g. ' /+w2 ' or ' /w10,s0 '.", text);
+			}	
+		}
+		
+	private static String getErrMessGE(int errCode, String text)
+
+		{
+		switch( errCode )
+			{
+		case ERR_PROX_MEAS_NULL:
+			return String.format("Abstandsoperator an der Stelle '%s': es fehlt eine der folgenden Angaben: w,s,p!", text);
+			
+		case ERR_PROX_MEAS_TOOGREAT:
+			return String.format("Abstandsoperator an der Stelle '%s': Bitte nur 1 der folgenden Angaben einsetzen: w,s,p! " +
+								 "Falls Mehrfachangabe erwünscht, müssen diese durch Kommata getrennt werden (z.B.: ' /+w2,s2,p0 ').", text);
+			
+		case ERR_PROX_VAL_NULL:
+			return String.format("Abstandsoperator an der Stelle '%s': Bitte einen numerischen Wert einsetzen (z.B. ' /+w5 ')! ", text);
+			
+		case ERR_PROX_VAL_TOOGREAT:
+			return String.format("Abstandsoperator an der Stelle '%s': Bitte nur 1 numerischen Wert einsetzen (z.B. ' /+w5 ')! ", text);
+			
+		case ERR_PROX_DIR_TOOGREAT:
+			return String.format("Abstandsoperator an der Stelle '%s': Bitte nur 1 Angabe '+' oder '-' oder keine! ", text);
+			
+		case ERR_PROX_WRONG_CHARS:
+			return String.format("Abstandsoperator an der Stelle '%s': unbekannte Abstandsoption(en)!", text);
+			
+		case ERR_LEM_WILDCARDS:
+			return String.format("Lemma-Suchbegriff an der Stelle '%s': Platzhalter (?*+) können im gesuchten Lemma nicht eingesetzt werden.", text);
+			
+		default:
+			return String.format("Abstandsoperator an der Stelle '%s': unbekannter Fehler. Korrekte Syntax z.B.: ' /+w2 ' oder ' /w10,s0 '.", text);
+			}
+		}
+	
+	/* getErrMess:
+	 * - returns error message depending of messLang.
+	 * 12.12.24/FB
+	 *  - moved to StatusCodes.java.
+	 */
+	
+	public static String getErrMess(int errCode, int messLang, String text)
+		
+		{
+		if( messLang == MLANG_GERMAN )
+			return getErrMessGE(errCode, text);
+		else
+			return getErrMessEN(errCode, text);	
+		}
+
 }
